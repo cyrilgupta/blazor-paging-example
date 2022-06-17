e31e6d345e2db42c5876566901d6d37cf856bbdf
Cyril Gupta <5773498+cyrilgupta@users.noreply.github.com>
2022-06-17 22:34:54 +0400

    update

---
+++ BlazorPaging/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using BlazorPaging.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorPaging.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int page)
+        {
+            return Enumerable.Range(page * 5 - 4, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
